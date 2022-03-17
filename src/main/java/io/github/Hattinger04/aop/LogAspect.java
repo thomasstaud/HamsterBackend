1d00796e12bbe233d8ed27edae89dbb668059bd3
Hattinger04 <s.8greiderer@gmail.com>
2022-03-17 20:21:38 +0100

    adding log4j and logoutLog (not working 100% yet)

--- src/main/java/io/github/Hattinger04/aop/LogAspect.java
+++ src/main/java/io/github/Hattinger04/aop/LogAspect.java
@@ -1,11 +1,12 @@
 package io.github.Hattinger04.aop;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,30 +16,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-	File file = new File("src/main/resources/hamster/logging/log.txt"); 
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    BufferedWriter writer;
-    Date date;
+	private static Logger logger;
+	static {
+		PatternLayout layout = new PatternLayout();
+		String conversionPattern = "%-7p %d [%t] %c %x - %m%n";
+		layout.setConversionPattern(conversionPattern);
 
-    
-	@Around(("execution(* io.github.Hattinger04.user.UserController.logoutPage(..))"))
-	public Object startControllerLog(ProceedingJoinPoint jp) throws Throwable {
-		Object result;
-		writer = new BufferedWriter(new FileWriter(file, true)); 
-    	for(Object o : jp.getArgs()) {
-    		System.out.println("Object: " + o.toString());
-    	}
-		try {
-			result = jp.proceed();
-			date = new Date(System.currentTimeMillis()); 
-			System.out.println("User logged out: " + jp.getSignature());
-			writer.append("Logged out at: " + formatter.format(date) + "\n"); // No username or smth like that yet
-			writer.close();
-			return result; 
-		} catch (Throwable e) {
-			System.out.println("Method abnormal termination: " + jp.getSignature());
-			e.printStackTrace();
-			throw e; 
-		}
+		ConsoleAppender consoleAppender = new ConsoleAppender();
+		consoleAppender.setLayout(layout);
+		consoleAppender.activateOptions();
+
+		FileAppender fileAppender = new FileAppender();
+		fileAppender.setFile("src/main/resources/log4j.log");
+		fileAppender.setLayout(layout);
+		fileAppender.activateOptions();
+
+		Logger rootLogger = Logger.getRootLogger();
+		rootLogger.setLevel(Level.DEBUG);
+		rootLogger.addAppender(consoleAppender);
+		rootLogger.addAppender(fileAppender);
+		
+		logger = Logger.getLogger(LogAspect.class); 
 	}
+
+	
+	@Around(("execution(* io.github.Hattinger04.user.UserController.logoutPage(..))"))
+	public Object logoutLog(ProceedingJoinPoint jp) throws Throwable {
+		Object o; 
+		HttpServletRequest request = (HttpServletRequest) jp.getArgs()[0]; 
+		String username = request.getRemoteUser(); 
+		System.out.println(username);
+		o = jp.proceed(); 
+		logger.info("User " + username + " logged out"); 
+		return o; 
+	}
+	
 }
