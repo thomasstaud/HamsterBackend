edc0f8847277ce861694d058d45ee91aa2f65317
Hattinger04 <s.8greiderer@gmail.com>
2022-04-30 13:50:14 +0200

    updating project

--- src/main/java/io/github/Hattinger04/hamster/HamsterController.java
+++ src/main/java/io/github/Hattinger04/hamster/HamsterController.java
@@ -1,7 +1,5 @@
 package io.github.Hattinger04.hamster;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
